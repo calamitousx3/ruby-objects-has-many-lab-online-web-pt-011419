76a5de12467a0162400ed68f06c7a9815d7fb60d
Carmen Renee <carmengenpt@gmail.com>
2019-02-17 00:03:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,8 @@ class Song
   end 
   
   def artist_name 
-    if self.author != nil  
-      self.author.name 
+    if self.artist != nil  
+      self.artist.name 
     else 
       nil 
     end 
